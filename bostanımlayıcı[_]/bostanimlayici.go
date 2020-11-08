2e823b29f753246a907d53a94521d06ccb335c84
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-11-08 12:40:29 +0300

    Update bostanimlayici.go

--- bostanımlayıcı[_]/bostanimlayici.go
+++ bostanımlayıcı[_]/bostanimlayici.go
@@ -1,1 +1,19 @@
+package main
 
+import "fmt"
+
+// _   = Bos Tanımlayıcı ...
+// hata , veri   = method() --- _ ,veri = method()
+
+func main() {
+
+	var _, x, _ int = 0, 10, 0
+	fmt.Println(x)
+
+	bos()
+}
+
+func bos() {
+	_, x, _ := 0, 20, 0
+	fmt.Println(x)
+}
