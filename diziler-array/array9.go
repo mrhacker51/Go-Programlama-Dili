aadfc0de158605abd151da4cc853e6a0bae1bfef
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-11-12 00:44:18 +0300

    Create array9.go

---
+++ diziler-array/array9.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var my_array = []string{}
+
+	my_array = append(my_array, "PHP")
+	fmt.Println(my_array)
+
+	my_array = append(my_array, "Golang")
+
+	fmt.Println(my_array)
+
+	//
+
+	var arry []string
+
+	fmt.Println(arry)
+
+	arry = append(arry, "CSharp")
+
+	fmt.Println(arry)
+
+	//
+
+}
