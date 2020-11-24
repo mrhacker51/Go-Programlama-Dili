05d0d04c9fbabe34f00b9498652b9f940890cbf3
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-11-25 02:23:56 +0300

    Create os_rename_and_mov.go

---
+++ Library-Usage/os_rename_and_mov.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	oldpath := "tester.txt"
+	newpath := "./news/test.txt"
+	err := os.Rename(oldpath, newpath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
